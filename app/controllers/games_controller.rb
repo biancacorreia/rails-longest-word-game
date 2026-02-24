6e61fe3ccb1c22ed75dd2cb9efb6d9fc7c6e0110
bianca correia <bi-a3@hotmail.com>
2026-02-24 08:42:12 +0000

    updated

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,26 +1,26 @@
-require 'open-uri'
-require 'json'
-
+equire "open-uri"
+require "json"
 class GamesController < ApplicationController
   def new
-    alphabet = ("A".."Z").to_a
-    @letters = Array.new(10) do alphabet.sample
+    @letters = Array.new(10) { ("A".."Z").to_a.sample }
+  end
+  def score
+    @word = params[:word]
+    @letters = params[:letters]
+    if !included?(@word, @letters)
+      @result = "Sorry but #{@word} can't be build out of #{@letters}"
+    elsif !english_word?(@word)
+      @result = "Sorry but #{@word} does not seem to be a valid English word."
+    else
+      @result = "Congratulations! #{@word} is a valid English word!"
     end
   end
-
-  def included?(guess, grid)
-  guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  def included?(word, grid)
+    word.chars.all? { |letter| word.count(letter) <= grid.count(letter) }
   end
-
   def english_word?(word)
-  response = URI.parse("https://dictionary.lewagon.com/#{word}")
-  json = JSON.parse(response.read)
-  return json['found']
-
-  def score (attempt, grid, time)
-    if included?&& enlgish_words (attempt.upcase, grid)
-      [score, "well done"]
-    else
-       "not an english word"
+    response = URI.parse("https://dictionary.lewagon.com/#{word}")
+    json = JSON.parse(response.read)
+    json["found"]
   end
 end
